02c3d949425699b7c892e11d14b80597c182fdc8
jeroenruytings@gmail.com <jeroenruytings@gmail.com@b8634888-6355-5a2e-0508-1dfa82acfde9>
2012-04-18 17:49:36 +0000

    Huntmethode geschreven, nog niet getest!

--- pcpacmanbackup/src/pacmansystem/ai/strategy/Hunt.java
+++ pcpacmanbackup/src/pacmansystem/ai/strategy/Hunt.java
@@ -1,7 +1,17 @@
 package pacmansystem.ai.strategy;
 
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
+
+import data.board.shortestpathfinder.dijkstra.DijkstraFinder;
+import data.board.shortestpathfinder.dijkstra.PathNotPossibleException;
+import data.enums.Direction;
+import data.enums.Orientation;
+import data.world.RobotData;
 
 import pacmansystem.ai.RobotController;
 
@@ -12,11 +22,16 @@ import pacmansystem.ai.RobotController;
  *
  */
 public class Hunt implements Strategy {
-
+	
 	private RobotController controller;
+	private HashMap<Point, Double> pvalues;
 	
 	public Hunt(RobotController controller) {
 		this.controller = controller;
+		pvalues = new HashMap<Point, Double>(); 
+		for(Point p : getController().getBoard().getPanels().keySet()){
+			pvalues.put(p,0.0);
+		}
 	}
 	
 	public RobotController getController() {
@@ -25,8 +40,28 @@ public class Hunt implements Strategy {
 	
 	@Override
 	public Queue<Point> constructRoute() {
-		//TODO
-		return null;
+		updatePValues();
+		Point destination = null;
+		double best = 0.0;
+		for(Point p : pvalues.keySet()){
+			double value = pvalues.get(p);
+			if(value > best){
+				best = value;
+				destination = p;
+			}
+		}
+		DijkstraFinder finder = new DijkstraFinder(getController().getData());
+		List<Point> path = null;
+		try {
+			path = finder.shortestPath(getController().getCurrentPoint(), destination);
+		} catch (PathNotPossibleException e) {
+			e.printStackTrace();
+		}
+		path.remove(path.size()-1);
+		Queue<Point> route = new LinkedList<Point>();
+		for (Point point: path)
+			route.add(point);
+		return route;
 	}
 
 	@Override
@@ -44,6 +79,53 @@ public class Hunt implements Strategy {
 	@Override
 	public boolean hasFinishedExploring() {
 		return true;
+	}
+	
+	//http://scalablegamedesign.cs.colorado.edu/wiki/Collaborative_Diffusion
+	private void updatePValues(){
+		resetPValues();
+		Point pacman = getController().getOwnData().getPacmanLastSighted();
+		ArrayList<Point> adapted = new ArrayList<Point>();
+		pvalues.put(pacman, 1000.0);
+		adapted.add(pacman);
+		boolean updatedSomething = true;
+		while(updatedSomething){
+			updatedSomething = false;
+			for(Point p : pvalues.keySet()){
+				if(adapted.contains(p))
+					continue;
+				double newValue = calculateP(p);
+				if(! (newValue == pvalues.get(p))){
+					updatedSomething = true;
+					adapted.add(p);
+					pvalues.put(p, newValue);
+				}
+			}
+		}
+	}
+
+	private void resetPValues() {
+		for(Point p : pvalues.keySet()){
+			pvalues.put(p, 0.0);
+		}
+	}
+
+	private double calculateP(Point p) {
+		double result = 0.0;
+		ArrayList<Point> otherGhosts = new ArrayList<Point>();
+		for(RobotData data : getController().getOtherBots()){
+			otherGhosts.add(data.getPosition());
+		}
+		for(Orientation orientation : Orientation.values()){
+			Point point = orientation.addTo(p);
+			if(otherGhosts.contains(point))
+				continue;
+			if(pvalues.containsKey(point)){
+				if(!getController().getBoard().wallBetween(p,point))
+					result += pvalues.get(point);
+			}
+		}
+		return 0.25*result;
 	}
 	
 }
